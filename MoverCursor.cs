22d8b95db689cf5213cd1220ae8a48551d4689f4
Aitordsgn <78177252+aitordsgn@users.noreply.github.com>
2021-09-08 19:31:06 +0200

    Mover cursor unity con animacion raton pulsado

---
+++ MoverCursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCursor : MonoBehaviour
+{
+    private SpriteRenderer Rend;
+    public Sprite Normal, Pulsado;
+    private void Start()
+    {
+        Cursor.visible = false;
+        Rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Rend.sprite = Pulsado;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Rend.sprite = Normal;
+        }
+    }
+}
